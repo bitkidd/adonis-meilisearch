319dd8e4686ee658bcc83b4510c7e43128126e0a
Chirill Ceban <cc@bitkidd.dev>
2021-08-05 15:19:02 +0300

    fix: add config types

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -10,6 +10,11 @@
 declare module '@ioc:Adonis/Addons/MeiliSearch' {
   import { MeiliSearch as MeilisearchContract } from 'meilisearch'
 
-  const Meilisearch: MeilisearchContract
-  export default Meilisearch
+  export interface MeiliSearchConfig {
+    host: string
+    apiKey?: string
+  }
+
+  const MeiliSearch: MeilisearchContract
+  export default MeiliSearch
 }
